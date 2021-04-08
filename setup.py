7627004aaa16611b3889d97c034c51b098f18b4f
nbvnhv.kjbjh nbvnb <tharakesh.iitp@gmail.com>
2021-04-08 22:00:55 +0530

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Wafer damage and life cycle predicton',
+    author='Darapu Tharakeswara Reddy',
+    license='MIT',
+)
